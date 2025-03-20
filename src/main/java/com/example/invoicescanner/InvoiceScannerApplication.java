d1408a52de2841ac593efbcad53c922281d76ceb
Daniel Reifmann <daniel.reifmann@taluma.de>
2025-03-20 15:37:16 +0100

    Init Commit

--- src/main/java/com/example/invoicescanner/InvoiceScannerApplication.java
+++ src/main/java/com/example/invoicescanner/InvoiceScannerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InvoiceScannerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InvoiceScannerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(InvoiceScannerApplication.class, args);
+    }
 
 }
--- src/test/java/com/example/invoicescanner/InvoiceScannerApplicationTests.java
+++ src/test/java/com/example/invoicescanner/InvoiceScannerApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class InvoiceScannerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
